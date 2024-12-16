042dc590d47869b351a70df0e340a25b190563b4
PeterC <petercalifano.gs@gmail.com>
2024-12-16 10:34:52 +0100

    Updated demo scripts

--- run_euroc.sh
+++ run_euroc.sh
@@ -1,1 +1,4 @@
-./scripts/stereoVIOEuroc.bash -p $HOME/devDir/SLAM-repos/kimera_system/V1_02_medium
+# Dataset input root folder
+dataset_abs_root=$1
+
+./scripts/stereoVIOEuroc.bash -p ${dataset_abs_root} #$HOME/devDir/SLAM-repos/kimera_system/V1_02_medium
